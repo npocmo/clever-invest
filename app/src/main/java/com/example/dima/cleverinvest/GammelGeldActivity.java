605eb39cd64604977b289d825e79f2d99c8c2109
Christian Delfs <christian.delfs@comdirect.de>
2017-10-05 13:47:45 +0200

    GammelGeldInit

---
+++ app/src/main/java/com/example/dima/cleverinvest/GammelGeldActivity.java
@@ -0,0 +1,14 @@
+package com.example.dima.cleverinvest;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GammelGeldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gammel_geld);
+    }
+}
