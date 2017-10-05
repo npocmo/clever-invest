020c560ed02b1ddab87fcd6ed478782820ebd613
Christian Delfs <christian.delfs@comdirect.de>
2017-10-05 22:53:07 +0200

    Investing Page finished

--- app/src/main/java/com/example/dima/cleverinvest/InvestActivity.java
+++ app/src/main/java/com/example/dima/cleverinvest/InvestActivity.java
@@ -20,9 +20,12 @@ import lecho.lib.hellocharts.view.LineChartView;
 public class InvestActivity extends AppCompatActivity {
 
     private LineChartView chart1;
+    private LineChartView chart2;
+    private int numberOfPointsMuster = 6;
     private int numberOfPoints = 6;
 
     float[] musterdepotValues = new float[120];
+    float[] depotValues = new float[120];
 
     private ValueShape shape = ValueShape.CIRCLE;
     @Override
@@ -31,10 +34,12 @@ public class InvestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_invest);
 
         chart1 = (LineChartView) findViewById(R.id.chart1);
+        chart2 = (LineChartView) findViewById(R.id.chart2);
 
         generateValues();
 
-        generateData();
+        generateDataChart1();
+        generateDataChart2();
 
         // Disable viewport recalculations, see toggleCubic() method for more info.
         chart1.setViewportCalculationEnabled(false);
@@ -138,15 +143,105 @@ public class InvestActivity extends AppCompatActivity {
         i++;
         musterdepotValues[i] = (float) 147;
         i++;
-        numberOfPoints = i;
+        numberOfPointsMuster = i;
+
+
+        int j = 0;
+        depotValues[j] = (float) 80;
+        j++;
+        depotValues[j] = (float) 81;
+        j++;
+        depotValues[j] = (float) 82;
+        j++;
+        depotValues[j] = (float) 82;
+        j++;
+        depotValues[j] = (float) 81;
+        j++;
+        depotValues[j] = (float) 81.22;
+        j++;
+        depotValues[j] = (float) 84;
+        j++;
+        depotValues[j] = (float) 85;
+        j++;
+        depotValues[j] = (float) 88;
+        j++;
+        depotValues[j] = (float) 91;
+        j++;
+        depotValues[j] = (float) 95;
+        j++;
+        depotValues[j] = (float) 100;
+        j++;
+        depotValues[j] = (float) 97;
+        j++;
+        depotValues[j] = (float) 110;
+        j++;
+        depotValues[j] = (float) 99;
+        j++;
+        depotValues[j] = (float) 97;
+        j++;
+        depotValues[j] = (float) 95;
+        j++;
+        depotValues[j] = (float) 96;
+        j++;
+        depotValues[j] = (float) 92;
+        j++;
+        depotValues[j] = (float) 90;
+        j++;
+        depotValues[j] = (float) 88;
+        j++;
+        depotValues[j] = (float) 89;
+        j++;
+        depotValues[j] = (float) 90;
+        j++;
+        depotValues[j] = (float) 91;
+        j++;
+        depotValues[j] = (float) 91.2;
+        j++;
+        depotValues[j] = (float) 89.9;
+        j++;
+        depotValues[j] = (float) 89.3;
+        j++;
+        depotValues[j] = (float) 88;
+        j++;
+        depotValues[j] = (float) 89;
+        j++;
+        depotValues[j] = (float) 87;
+        j++;
+        depotValues[j] = (float) 93;
+        j++;
+        depotValues[j] = (float) 94;
+        j++;
+        depotValues[j] = (float) 95;
+        j++;
+        depotValues[j] = (float) 94;
+        j++;
+        depotValues[j] = (float) 95;
+        j++;
+        depotValues[j] = (float) 96;
+        j++;
+        depotValues[j] = (float) 95.5;
+        j++;
+        depotValues[j] = (float) 94.5;
+        j++;
+        depotValues[j] = (float) 93;
+        j++;
+        depotValues[j] = (float) 94;
+        j++;
+        depotValues[j] = (float) 92;
+        j++;
+        depotValues[j] = (float) 95.4;
+        j++;
+        numberOfPoints = j;
+
+
     }
 
-    private void generateData() {
+    private void generateDataChart1() {
 
         List<Line> lines = new ArrayList<Line>();
 
         List<PointValue> values = new ArrayList<PointValue>();
-        for (int j = 0; j < numberOfPoints; ++j) {
+        for (int j = 0; j < numberOfPointsMuster; ++j) {
             values.add(new PointValue(j, musterdepotValues[j]));
         }
 
@@ -202,6 +297,70 @@ public class InvestActivity extends AppCompatActivity {
 
         data.setBaseValue(Float.NEGATIVE_INFINITY);
         chart1.setLineChartData(data);
+    }
+
+
+    private void generateDataChart2() {
+
+        List<Line> lines = new ArrayList<Line>();
+
+        List<PointValue> values = new ArrayList<PointValue>();
+        for (int j = 0; j < numberOfPoints; ++j) {
+            values.add(new PointValue(j, depotValues[j]));
+        }
+
+        Line line = new Line(values);
+        line.setColor(ContextCompat.getColor(getApplicationContext(), R.color.bright_yellow));
+        line.setShape(shape);
+        boolean isCubic = false;
+        line.setCubic(isCubic);
+        boolean isFilled = true;
+        line.setFilled(isFilled);
+        boolean hasLabels = false;
+        line.setHasLabels(hasLabels);
+        boolean hasLabelForSelected = false;
+        line.setHasLabelsOnlyForSelected(hasLabelForSelected);
+        boolean hasLines = true;
+        line.setHasLines(hasLines);
+        //line.setHasPoints(hasPoints);
+        line.setHasPoints(false);
+        lines.add(line);
+
+        LineChartData data = new LineChartData(lines);
+
+        boolean hasAxes = true;
+        if (hasAxes) {
+            Axis axisX = new Axis();
+
+            axisX.setValues(null);
+            axisX.setHasSeparationLine(true);
+            axisX.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.dark));
+            axisX.setHasLines(true);
+            axisX.setLineColor(ContextCompat.getColor(getApplicationContext(), R.color.dark));
+            axisX.setHasTiltedLabels(true);
+            axisX.setTextSize(14);
+
+            axisX.setFormatter(new SimpleAxisValueFormatter().setAppendedText(". Month".toCharArray()));//.setAppendedText("km".toCharArray()));
+
+            Axis axisY = new Axis();
+            axisY.setHasLines(true);
+            axisY.setLineColor(ContextCompat.getColor(getApplicationContext(), R.color.dark));
+            axisY.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.dark));
+            axisY.setHasSeparationLine(true);
+            //if (hasAxesNames) {
+            if (false) {
+                axisX.setName("");
+                axisY.setName("");
+            }
+            data.setAxisXBottom(axisX);
+            data.setAxisYLeft(axisY);
+        } else {
+            data.setAxisXBottom(null);
+            data.setAxisYLeft(null);
+        }
+
+        data.setBaseValue(Float.NEGATIVE_INFINITY);
+        chart2.setLineChartData(data);
     }
 
 }
