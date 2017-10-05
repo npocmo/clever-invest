824d8d2e6fa7927bf543f59261157f6b277e16c0
Jessica.Keil <Jessica.Keil@comdirect.de>
2017-10-05 17:38:09 +0200

    gemergtes wieder repariert

--- app/src/main/java/com/example/dima/cleverinvest/GammelGeldActivity.java
+++ app/src/main/java/com/example/dima/cleverinvest/GammelGeldActivity.java
@@ -1,10 +1,10 @@
 package com.example.dima.cleverinvest;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
