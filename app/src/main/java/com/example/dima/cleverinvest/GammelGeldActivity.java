2fc4ac3b7241b844887698ac528d207990388378
Christian Delfs <christian.delfs@comdirect.de>
2017-10-05 15:44:54 +0200

    chart

--- app/src/main/java/com/example/dima/cleverinvest/GammelGeldActivity.java
+++ app/src/main/java/com/example/dima/cleverinvest/GammelGeldActivity.java
@@ -1,14 +1,215 @@
 package com.example.dima.cleverinvest;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.formatter.SimpleAxisValueFormatter;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.ValueShape;
+import lecho.lib.hellocharts.view.LineChartView;
 
 public class GammelGeldActivity extends AppCompatActivity {
+
+    private LineChartView chart;
+    private LineChartData data;
+    private int numberOfLines = 1;
+    private int maxNumberOfLines = 4;
+    private int numberOfPoints = 12;
+
+    float[][] randomNumbersTab = new float[maxNumberOfLines][120];
+
+    private boolean hasAxes = true;
+    private boolean hasAxesNames = true;
+    private boolean hasLines = true;
+    private boolean hasPoints = true;
+    private ValueShape shape = ValueShape.CIRCLE;
+    private boolean isFilled = true;
+    private boolean hasLabels = false;
+    private boolean isCubic = false;
+    private boolean hasLabelForSelected = false;
+    private boolean pointsHaveDifferentColor;
+    private boolean hasGradientToTransparent = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gammel_geld);
+
+        chart = (LineChartView) findViewById(R.id.chart);
+
+        generateValues();
+
+        generateData();
+
+        // Disable viewport recalculations, see toggleCubic() method for more info.
+        chart.setViewportCalculationEnabled(false);
+
+        //resetViewport();
+
     }
+
+    @Override
+    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+
+        return super.onCreateView(parent, name, context, attrs);
+    }
+
+    private void generateValues() {
+        //for (int i = 0; i < maxNumberOfLines; ++i) {
+            //for (int j = 0; j < numberOfPoints; ++j) {
+                //randomNumbersTab[i][j] = (float) Math.random() * 100f;
+            //}
+        //}
+        int i = 0;
+        randomNumbersTab[0][i] = (float) 123;
+        i++;
+        randomNumbersTab[0][i] = (float) 124;
+        i++;
+        randomNumbersTab[0][i] = (float) 111;
+        i++;
+        randomNumbersTab[0][i] = (float) 126;
+        i++;
+        randomNumbersTab[0][i] = (float) 127;
+        i++;
+        randomNumbersTab[0][i] = (float) 128;
+        i++;
+        randomNumbersTab[0][i] = (float) 118;
+        i++;
+        randomNumbersTab[0][i] = (float) 119;
+        i++;
+        randomNumbersTab[0][i] = (float) 130;
+        i++;
+        randomNumbersTab[0][i] = (float) 129;
+        i++;
+        randomNumbersTab[0][i] = (float) 133;
+        i++;
+        randomNumbersTab[0][i] = (float) 119;
+        i++;
+        randomNumbersTab[0][i] = (float) 130;
+        i++;
+        randomNumbersTab[0][i] = (float) 129;
+        i++;
+        randomNumbersTab[0][i] = (float) 133;
+        i++;
+        randomNumbersTab[0][i] = (float) 119;
+        i++;
+        randomNumbersTab[0][i] = (float) 130;
+        i++;
+        randomNumbersTab[0][i] = (float) 129;
+        i++;
+        randomNumbersTab[0][i] = (float) 133;
+        i++;
+        randomNumbersTab[0][i] = (float) 119;
+        i++;
+        randomNumbersTab[0][i] = (float) 130;
+        i++;
+        randomNumbersTab[0][i] = (float) 129;
+        i++;
+        randomNumbersTab[0][i] = (float) 133;
+        i++;
+        randomNumbersTab[0][i] = (float) 119;
+        i++;
+        randomNumbersTab[0][i] = (float) 130;
+        i++;
+        randomNumbersTab[0][i] = (float) 129;
+        i++;
+        randomNumbersTab[0][i] = (float) 133;
+        i++;
+        randomNumbersTab[0][i] = (float) 119;
+        i++;
+        randomNumbersTab[0][i] = (float) 130;
+        i++;
+        randomNumbersTab[0][i] = (float) 129;
+        i++;
+        randomNumbersTab[0][i] = (float) 133;
+        i++;
+        randomNumbersTab[0][i] = (float) 119;
+        i++;
+        randomNumbersTab[0][i] = (float) 130;
+        i++;
+        randomNumbersTab[0][i] = (float) 129;
+        i++;
+        randomNumbersTab[0][i] = (float) 133;
+        i++;
+        randomNumbersTab[0][i] = (float) 119;
+        i++;
+        randomNumbersTab[0][i] = (float) 130;
+        i++;
+        randomNumbersTab[0][i] = (float) 129;
+        i++;
+        randomNumbersTab[0][i] = (float) 133;
+        i++;
+
+        numberOfPoints = i;
+    }
+
+    private void generateData() {
+
+        List<Line> lines = new ArrayList<Line>();
+        for (int i = 0; i < numberOfLines; ++i) {
+
+            List<PointValue> values = new ArrayList<PointValue>();
+            for (int j = 0; j < numberOfPoints; ++j) {
+                values.add(new PointValue(j, randomNumbersTab[i][j]));
+            }
+
+            Line line = new Line(values);
+            line.setColor(ContextCompat.getColor(getApplicationContext(), R.color.bright_yellow));
+            line.setShape(shape);
+            line.setCubic(isCubic);
+            line.setFilled(isFilled);
+            line.setHasLabels(hasLabels);
+            line.setHasLabelsOnlyForSelected(hasLabelForSelected);
+            line.setHasLines(hasLines);
+            //line.setHasPoints(hasPoints);
+            line.setHasPoints(false);
+            if (pointsHaveDifferentColor) {
+                line.setPointColor(ContextCompat.getColor(getApplicationContext(), R.color.bright_yellow));
+            }
+            lines.add(line);
+        }
+
+        data = new LineChartData(lines);
+
+        if (hasAxes) {
+            Axis axisX = new Axis();
+            axisX.setHasSeparationLine(true);
+            axisX.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.dark));
+            axisX.setHasLines(true);
+            axisX.setLineColor(ContextCompat.getColor(getApplicationContext(), R.color.dark));
+
+            axisX.setFormatter(new SimpleAxisValueFormatter().setPrependedText("test".toCharArray()));//.setAppendedText("km".toCharArray()));
+
+            Axis axisY = new Axis();
+            axisY.setHasLines(true);
+            axisY.setLineColor(ContextCompat.getColor(getApplicationContext(), R.color.dark));
+            axisY.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.dark));
+            axisY.setHasSeparationLine(true);
+            //if (hasAxesNames) {
+            if (false) {
+                axisX.setName("");
+                axisY.setName("");
+            }
+            data.setAxisXBottom(axisX);
+            data.setAxisYLeft(axisY);
+        } else {
+            data.setAxisXBottom(null);
+            data.setAxisYLeft(null);
+        }
+
+        data.setBaseValue(Float.NEGATIVE_INFINITY);
+        chart.setLineChartData(data);
+    }
+
 }
